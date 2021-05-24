95ebe20b885162aacb6da7fd3422c74be7f01e7b
Edward Nagai <81211274+edward3923@users.noreply.github.com>
2021-05-24 18:54:48 -0500

    firebase settings

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyCKJoxlZ3hViRPGjaw3JAagojoRHD4GfQc",
+  authDomain: "kiei-451-final-b3fd4.firebaseapp.com",
+  projectId: "kiei-451-final-b3fd4",
+  storageBucket: "kiei-451-final-b3fd4.appspot.com",
+  messagingSenderId: "886508661968",
+  appId: "1:886508661968:web:f5b8e7f6be2b9e7e0942d5"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
